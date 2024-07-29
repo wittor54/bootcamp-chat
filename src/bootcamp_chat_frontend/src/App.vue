c611ce73b0bb7e88f33f365e9904e5804dd6bd47
wittor54 <xdwittor@gmail.com>
2024-07-29 17:54:09 +0200

    sdfsdf

--- src/bootcamp_chat_frontend/src/App.vue
+++ src/bootcamp_chat_frontend/src/App.vue
@@ -2,26 +2,25 @@
 import { ref } from 'vue';
 import { bootcamp_chat_backend } from '../../declarations/bootcamp_chat_backend';
 
-
 export default {
   data() {
     return {
-      newNote: ""
+      newNote: "",
       notes: [] as string[]
     }
   },
   methods: {
-    async didajNotatke(){
+    async dodajNotatke() {
       await bootcamp_chat_backend.add_note(this.newNote)
-      await this.pobierz_notatki()
+      await this.pobierzNotatki()
     },
-    async pobierz_notatki(){
+    async pobierzNotatki() {
       this.notes = await bootcamp_chat_backend.get_notes()
     }
   },
-  mounted() {
-    this.pobierz_notatki()
-  },
+  mounted(){
+    this.pobierzNotatki()
+  }
 }
 </script>
 
@@ -30,11 +29,11 @@ export default {
     <img src="/logo2.svg" alt="DFINITY logo" />
     <br />
     <br />
-    <div>
-        {{newNote}}
+      <div>
+      {{ notes }}
     </div>
     <div>
-        <textarea v-model="newNote"></textarea><button @click="dodajNotatke">dodaj notatke</button>
+      <textarea v-model="newNote"></textarea><button @click="dodajNotatke">Dodaj notatke</button>
     </div>
   </main>
 </template>
