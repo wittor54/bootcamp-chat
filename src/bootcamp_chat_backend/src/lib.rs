0e9fbcce691d516d412a513cbcd775d276065120
wittor54 <xdwittor@gmail.com>
2024-07-30 20:01:26 +0200

    dodano chat, login, kilku uzytkownikow

--- src/bootcamp_chat_backend/src/lib.rs
+++ src/bootcamp_chat_backend/src/lib.rs
@@ -1,32 +1,35 @@
-use std::{cell::{Ref, RefCell}, collections::HashMap};
+use std::{cell:: RefCell, collections::HashMap};
 
 use candid::Principal;
 use ic_cdk::caller;
 
 thread_local! {
-    static NOTES: RefCell<HashMap<Principal, Vec<String>>> = RefCell::default();
+    static CHAT: RefCell<HashMap<[Principal; 2], Vec<String>>> = RefCell::default();
 }
 
 #[ic_cdk::query]
-fn get_notes(user: Principal) -> Option<Vec<String>> {
-    NOTES.with_borrow(|notes|  notes.get(&user).cloned())
+fn get_chat(chat_path: [Principal; 2]) -> Option<Vec<String>> {
+    CHAT.with_borrow(|chats|  chats.get(&chat_path).cloned())
 }
 
 #[ic_cdk::update]
-fn add_note(note: String) {
-    let user = caller();
+fn add_chat_msg(msg: String, user2: Principal) {
+    let user1 = caller();
 
-    if user == Principal::anonymous() {
+    if user1 == Principal::anonymous() {
         panic!("Anonymous Principal!")
     }
 
-    NOTES.with_borrow_mut(|notes| {
-        let mut_notes = notes.get_mut(&user);
+    let mut principals = [user1, user2];
+    principals.sort();
 
-        if let Some(notes_vec) = mut_notes {
-            notes_vec.push(note);
+    CHAT.with_borrow_mut(|chats| {
+        let mut_chat = chats.get_mut(&principals);
+
+        if let Some(chat_msgs) = mut_chat {
+            chat_msgs.push(msg);
         } else {
-            notes.insert(user, vec![note]);
+            chats.insert(principals, vec![msg]);
         }
     })
 }
--- src/bootcamp_chat_frontend/src/App.vue
+++ src/bootcamp_chat_frontend/src/App.vue
@@ -1,42 +1,77 @@
 <script lang="ts">
 import { ref } from 'vue';
-import { bootcamp_chat_backend } from '../../declarations/bootcamp_chat_backend';
+import { bootcamp_chat_backend, canisterId, createActor } from '../../declarations/bootcamp_chat_backend';
 import { AuthClient } from '@dfinity/auth-client';
 import { HttpAgent } from '@dfinity/agent';
 import type { Identity } from '@dfinity/agent';
+import { Principal } from '@dfinity/principal';
 
 export default {
   data() {
     return {
-      newNote: "",
-      notes: [] as string[],
+      newChat: "",
+      chats: [] as string[][],
       identity: undefined as undefined | Identity,
+      principal: undefined as undefined | Principal,
+      targetPrincipal: "",
     }
   },
   methods: {
-    async dodajNotatke() {
-      await bootcamp_chat_backend.add_note(this.newNote)
-      await this.pobierzNotatki()
+    isUserLogged() {
+      if (!this.identity || !this.principal || this.principal === Principal.anonymous()) {
+        throw new Error("PLZ log in")
+      }
+      return {
+        identity: this.identity,
+        principal: this.principal 
+      }
     },
-    async pobierzNotatki() {
-      this.notes = await bootcamp_chat_backend.get_notes()
+    validateTargetPrincipal(){
+      const cleanTargetPrincipal = this.targetPrincipal.trim();
+      if (cleanTargetPrincipal === ""){
+        throw new Error("No principal")
+      }
+      const targetPrincipal = Principal.fromText(cleanTargetPrincipal)
+      if (!targetPrincipal || targetPrincipal === Principal.anonymous()){
+        throw new Error("Wrong target")
+      }
+      return targetPrincipal
+    },
+    getAuthClient(){
+      this.isUserLogged()
+      return createActor(canisterId, {
+        agentOptions: {
+          identity: this.identity
+        }
+      });
+    },
+    async dodajChatMSG() {
+      const targetPrincipal = this.validateTargetPrincipal()
+      const backend = this.getAuthClient();
+      await backend.add_chat_msg(this.newChat, targetPrincipal)
+      await this.pobierzChaty()
+    },
+    async pobierzChaty() {
+      const {identity, principal} = this.isUserLogged()
+      const targetPrincipal = this.validateTargetPrincipal()
+
+      const chatPath = [targetPrincipal, identity.getPrincipal()].sort()
+      this.chats = await bootcamp_chat_backend.get_chat(chatPath)
     },
     async login() {
       const authClient = await AuthClient.create();
       await authClient.login({
-        identityProvider: "http://by6od-j4aaa-aaaaa-qaadq-cai.localhost:4943/"
+        identityProvider: "http://by6od-j4aaa-aaaaa-qaadq-cai.localhost:4943/",
+        onSuccess: async () => {
+          const identity = authClient.getIdentity();
+          this.principal = identity.getPrincipal();
+          this.identity = identity;
+          console.log("Zalogowano", this.principal)
+          await this.pobierzChaty()
+        }
       })
-
-      const identity = authClient.getIdentity();
-      console.log("Zalogowano", identity.getPrincipal())
-      this.identity = identity;
-      
-     //const agent = new HttpAgent({ identity });
     }
   },
-  mounted(){
-    this.pobierzNotatki()
-  }
 }
 </script>
 
@@ -45,12 +80,17 @@ export default {
     <img src="/logo2.svg" alt="DFINITY logo" />
     <br />
     <br />
-    {{ identity?.getPrincipal() }} <button @click="login">Login</button>
+    {{ principal }} <button @click="login">login</button>
     <div>
-      {{ notes }}
+      <input v-model="targetPrincipal" /><button @click="pobierzChaty">pobierz chat</button>
     </div>
     <div>
-      <textarea v-model="newNote"></textarea><button @click="dodajNotatke">Dodaj notatke</button>
+      <div v-for="chat in chats[0]">
+        {{ chat }}
+      </div>
+    </div>
+    <div>
+      <textarea v-model="newChat"></textarea><button @click="dodajChatMSG">Dodaj notatke</button>
     </div>
   </main>
 </template>
